399b625ba6720f12011e654be3df6aeb5e291424
BryanSigala <82849413+BryanSigala@users.noreply.github.com>
2024-10-03 17:29:27 -0600

    Add logo to navbar component

--- src/components/ui/navbar.jsx
+++ src/components/ui/navbar.jsx
@@ -1,6 +1,15 @@
+import Image from 'next/image';
+
+
 export default function Nav() {
     return (
         <nav>
+            <Image 
+                src="/images/citc_logo_y_texto.jpg"
+                alt="CITC Logo"
+                width={250} 
+                height={50}
+            />
             Aqui va el NAV
         </nav>
     );
