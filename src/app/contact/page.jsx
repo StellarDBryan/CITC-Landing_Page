178fa6c597fc10d59a673ff6fa0b1a772af747e9
Erica Rodríguez <erica_rdzb@hotmail.com>
2024-10-03 20:29:52 -0600

    chore: Add @tailwindcss/forms plugin and update dependencies

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,18 @@
+import Form from '@/components/form';
+import Nav from "@/components/ui/navbar.jsx";
+
+export default function Contact() {
+
+    return (
+        <>
+            <div>
+                <header>
+                    <Nav />
+                </header>
+                <main>
+                    <Form />
+                </main>
+            </div>
+        </>
+    );
+}
