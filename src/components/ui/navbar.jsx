2190e63403474453eacb26eac70cb40f991afbcd
BryanSigala <82849413+BryanSigala@users.noreply.github.com>
2024-10-03 19:59:24 -0600

    Fix spacing between buttons on navbar

--- src/components/ui/navbar.jsx
+++ src/components/ui/navbar.jsx
@@ -4,30 +4,30 @@ import Image from 'next/image';
 export default function Nav() {
     return (
         <>
-            <nav className="bg-gray-clear-citc py-6 items-center flex flex-row">
+            <nav className="bg-gray-clear-citc py-6 px-3 items-center flex flex-row">
                 <Image 
                     src="/images/citc_logo_y_texto.jpg" 
                     alt="CITC Logo" 
                     width={250} 
                     height={50}
                 />
-                <div className="px-10 h-full w-full border-2 border-solid flex flex-row items-center justify-center space-x-8">
+                <div className="px-10 h-full w-full border-2 border-solid flex flex-row items-center justify-center space-x-20">
                     <button className="text-gray-dark-citc h-auto w-auto px-2 border-2 border-solid">
                         Inicio
                     </button>
-                    <button className="text-gray-dark-citc h-auto w-auto px-2 border-2 border-solid">
+                    <button className="text-gray-dark-citc h-full w-auto px-2 border-2 border-solid">
                         Servicios
                     </button>
-                    <button className="text-gray-dark-citc h-auto w-auto px-2 border-2 border-solid">
+                    <button className="text-gray-dark-citc h-full w-auto px-2 border-2 border-solid">
                         Afiliaciones
                     </button>
-                    <button className="text-gray-dark-citc h-auto w-auto px-2 border-2 border-solid">
+                    <button className="text-gray-dark-citc h-full w-auto px-2 border-2 border-solid">
                         Eventos
                     </button>
-                    <button className="text-gray-dark-citc h-auto w-auto px-2 border-2 border-solid">
+                    <button className="text-gray-dark-citc h-full w-auto px-2 border-2 border-solid">
                         Noticias
                     </button>
-                    <button className="text-gray-dark-citc h-auto w-auto px-2 border-2 border-solid">
+                    <button className="text-gray-dark-citc h-full w-auto px-2 border-2 border-solid">
                         Contacto
                     </button>
                 </div>
