0a68a515382ffe650aef7028e15376185d5fbe56
BryanSigala <bryansigala@hotmail.com>
2024-10-25 01:30:07 -0600

    add cuu images to homepage

--- src/components/homepage/home.jsx
+++ src/components/homepage/home.jsx
@@ -4,16 +4,19 @@ export default function Home(){
 
     return (
         <>
-            <section className="bg-gray-700 h-screen">
+            <section className="bg-gray-700 bg-[url('/images/cuu_2.jpg')] bg-cover bg-center h-screen">
                 <div>
                     <Image src="/images/citc_logos/CITC_horiz-04.png"
                         alt="CITC Logo"
-                        width={673}
+                        width={600}
                         height={250}/>
                     <h2 className="font-medium text-h4 text-gray-clear-citc">
                         Colaboración y Evolución Tecnológica <br /> en el corazón de Chihuahua
                     </h2>
                 </div>
+            </section>
+            <section className="my-10">
+                <h1 className="text-h1">PRUEBA DE SCROLL</h1>
             </section>
         </>
     );
