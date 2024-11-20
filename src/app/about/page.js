b09b2c26606f290d3c6b7bfef3b14d61deae1647
BryanSigala <bryansigala@hotmail.com>
2024-11-20 00:08:12 -0600

    add components

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+import About, { Associates } from "@/components/about";
+
+export default function AboutUs(){
+
+    return(
+        <>
+            <About />
+            <Associates />
+        </>
+    );
+}
