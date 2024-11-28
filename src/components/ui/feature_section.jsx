83027d3b13597ab3764b2f9313c0c9a1e70aaa1c
BryanSigala <bryansigala@hotmail.com>
2024-11-28 14:55:49 -0600

    style: remove dark theme style in featureSection component

--- src/components/ui/feature_section.jsx
+++ src/components/ui/feature_section.jsx
@@ -21,32 +21,32 @@ const Feature = ({
   return (
     (<div
       className={cn(
-        "flex flex-col lg:border-r  py-10 relative group/feature dark:border-neutral-800",
-        (index === 0 || index === 4) && "lg:border-l dark:border-neutral-800",
-        index < 4 && "lg:border-b dark:border-neutral-800"
+        "flex flex-col lg:border-r  py-10 relative group/feature ",
+        (index === 0 || index === 4) && "lg:border-l ",
+        index < 4 && "lg:border-b "
       )}>
       {index < 4 && (
         <div
-          className="opacity-0 group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-t from-neutral-100 dark:from-neutral-800 to-transparent pointer-events-none" />
+          className="opacity-0 group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-t from-neutral-100 to-transparent pointer-events-none" />
       )}
       {index >= 4 && (
         <div
-          className="opacity-0 group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-b from-neutral-100 dark:from-neutral-800 to-transparent pointer-events-none" />
+          className="opacity-0 group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-b from-neutral-100 to-transparent pointer-events-none" />
       )}
       <div
-        className="mb-4 relative z-10 px-10 text-neutral-600 dark:text-neutral-400">
+        className="mb-4 relative z-10 px-10 text-neutral-600 ">
         {icon}
       </div>
       <div className="text-h6 font-bold mb-2 relative z-10 px-10">
         <div
-          className="absolute left-0 inset-y-0 h-6 group-hover/feature:h-8 w-1 rounded-tr-full rounded-br-full bg-neutral-300 dark:bg-neutral-700 group-hover/feature:bg-blue-500 transition-all duration-200 origin-center" />
+          className="absolute left-0 inset-y-0 h-6 group-hover/feature:h-8 w-1 rounded-tr-full rounded-br-full bg-neutral-300 group-hover/feature:bg-blue-500 transition-all duration-200 origin-center" />
         <span
-          className="group-hover/feature:translate-x-2 group-hover/feature:text-blue-dark-citc transition duration-200 inline-block text-neutral-800 dark:text-neutral-100">
+          className="group-hover/feature:translate-x-2 group-hover/feature:text-blue-dark-citc transition duration-200 inline-block text-neutral-800">
           {title}
         </span>
       </div>
       <p
-        className="text-regular text-neutral-600 dark:text-neutral-300 max-w-xs relative z-10 px-10">
+        className="text-regular text-neutral-600 max-w-xs relative z-10 px-10">
         {description}
       </p>
     </div>)
