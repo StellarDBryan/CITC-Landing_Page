53290c123fb029dcef35e45fea53446d5b0d33a4
BryanSigala <bryansigala@hotmail.com>
2025-01-14 13:38:16 -0600

    style: fix color and font size in Form component for Contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,12 +1,36 @@
 import ContactForm from '@/components/forms';
 
+const content = {
+    title: "Construyamos juntos el futuro", 
+    description: "Déjanos tus datos y nos pondremos en contacto contigo para comenzar esta gran colaboración.", 
+    firstName: "Nombre", 
+    lastName: "Apellido", 
+    email: "Correo", 
+    company: "Compañia/Organización", 
+    phone: "Teléfono", 
+    message: "Mensaje", 
+    message_placeholder: "Cuéntanos cómo te gustaría colaborar", 
+    button: "Enviar", 
+    summited: "Enviado", 
+    summit_succed: "Formulario enviado con éxito.", 
+    error: "Error al enviar el formulario", 
+    verify: {
+        fisrtName: "Nombre no válido (solo letras, máximo 30 caracteres).\n",
+        lastName: "Apellido no válido (solo letras, máximo 30 caracteres).\n", 
+        email: "Correo no válido.\n", 
+        company: "Compañia no puede tener más de 50 caracteres.\n", 
+        phone: "Teléfono no válido (solo números, máximo 15 caracteres).\n", 
+        message: "Mensaje es obligatorio.\n",  
+    }, 
+}; 
+
 export default function Contact() {
 
     return (
         <>
             <div>
                 <main>
-                    <ContactForm />
+                    <ContactForm content={content} />
                 </main>
             </div>
         </>
